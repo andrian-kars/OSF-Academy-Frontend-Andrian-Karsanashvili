25ac50cb070132750a7ba07038428a6a23106eb8
pmAndrian <66482484+pmAndrian@users.noreply.github.com>
2020-06-13 10:05:09 +0300

    fully responsible home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,22 @@
 $(function () {
+    // ----- Menu Button -----
+    const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+        if (!menuOpen) {
+            menuBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+        }
+    });
 
-    // // ----- Drop-down Menu -----
-    // let dropMain = document.getElementsByClassName('navigation__item');
-    // let dropChosen = document.getElementsByClassName('chosen-value__item_main');
-
-    // for (let i = 0; i < dropMain.length; i++) {
-    //     dropMain[i].addEventListener('mouseenter', showSub, false);
-    //     dropMain[i].addEventListener('mouseleave', hideSub, false);
-    // }
-
-    // for (let i = 0; i < dropChosen.length; i++) {
-    //     dropChosen[i].addEventListener('mouseenter', showSub, false);
-    //     dropChosen[i].addEventListener('mouseleave', hideSub, false);
-    // }
-
-    // function showSub() {
-    //     if (this.children.length > 1) {
-    //         // this.children[1].style.height = 'auto';
-    //         // this.children[1].style.opacity = '1';
-    //         // this.children[1].style.overflow = 'visible';
-    //         this.children[1].style.display = 'flex';
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
-    // function hideSub() {
-    //     if (this.children.length > 1) {
-    //         // this.children[1].style.height = '0';
-    //         // this.children[1].style.opacity = '0';
-    //         // this.children[1].style.overflow = 'hidden';
-    //         this.children[1].style.display = 'none';
-    //     } else {
-    //         return false;
-    //     }
-    // }
-    // // ----- // Drop-down Menu -----
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault;
+        $('.navigation').toggleClass('navigation_active');
+    });
+    // ----- // Menu Button -----
 
     // ----- Slick Slider -----
     $('.slider-slick__slider').slick({
@@ -87,8 +68,6 @@ $(function () {
     new Date().getFullYear();
     document.getElementById("year").innerHTML = new Date().getFullYear();
     // ----- // Current Year -----
-
-
 
 
 
